e659750f17d180a8014da00924e586067a87db52
nguiard <nguiard@student.42.fr>
2022-11-23 14:28:12 +0100

    version finale

--- main.d
+++
@@ -1,7 +0,0 @@
-main.o: main.cpp set.hpp utils_header/rb_tree_set.hpp \
-  utils_header/iterator.hpp utils_header/pair.hpp utils_header/equal.hpp \
-  utils_header/lexicographical_compare.hpp map.hpp \
-  utils_header/rb_tree_map.hpp utils_header/map_iterator.hpp \
-  utils_header/reverse_iterator.hpp vector.hpp \
-  utils_header/enable_if.hpp utils_header/is_integral.hpp \
-  utils_header/vector_iterator.hpp stack.hpp
--- todo.md
+++
@@ -1,6 +0,0 @@
-## Fix Output:
-
-- [ ] rite_eq_ope
-- [x] rite2
-- [x] relational_ope
-- [x] ite_eq_ope
